895e23e1ce29f3794a96526f4db0815abef69d5c
Igres111 <sergikaralashvili@gmail.com>
2024-07-17 18:18:11 +0400

    wip structure

---
+++ src/js comps/Content.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Content(props) {
+  return (
+    <div>
+      <p>num</p>
+      <p>name</p>
+      <p>date</p>
+      <p>amount</p>
+      <p>state</p>
+    </div>
+  );
+}
+
+export default Content;
